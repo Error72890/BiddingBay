2b71783e92c721ce3d7d0fba75f8d2291b5afb63
Vicente Nava <vicnav28@gmail.com>
2024-05-23 17:35:06 -0600

    chore: Update auction creator component styles and form layout

--- src/app/components/auction-creator/auction-creator.component.ts
+++ src/app/components/auction-creator/auction-creator.component.ts
@@ -1,89 +1,130 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
-import { SessionGuard } from 'src/app/guards/session.guard';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Auction {
+  objectName: string;
+  description: string;
+  categories: string[];
+  startDate: string;
+  endDate: string;
+  startBid: number;
+  img: File | null;
+}
 
 @Component({
-  selector: 'app-auction-creator',
+  selector: 'auction-creator',
   templateUrl: './auction-creator.component.html',
   styleUrls: ['./auction-creator.component.scss'],
-  providers: [SessionGuard]
 })
 export class AuctionCreatorComponent {
-  imageSrc: HTMLImageElement | ArrayBuffer | null = null; // Property for holding image source
-  imageName: string | null = null; // Property for holding image name
+  @ViewChild('fileInput') fileInput: ElementRef<HTMLInputElement> | undefined;
+  public auction: Auction = {
+    objectName: '',
+    description: '',
+    categories: [],
+    startDate: '',
+    endDate: '',
+    startBid: 0,
+    img: null,
+  };
+  public categories: string[] = ['Category 1', 'Category 2', 'Category 3']; // Define las categorías disponibles
+  public selectedCategories: { [key: string]: boolean } = {};
 
-  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
+  public imageSrc: string | ArrayBuffer | null = null;
 
-  readURL(input: HTMLInputElement) {
-    if (input.files && input.files[0]) {
-      const reader: FileReader = new FileReader();
+  constructor(private http: HttpClient) { }
   
-      reader.onload = (e: any) => {
-        this.renderer.addClass(
-          this.elementRef.nativeElement.querySelector('.image-upload-wrap'),
-          'd-none'
-        );
+  getCurrentDateTime(): string {
+    return new Date().toISOString().slice(0, 16);
 
-        if (typeof e.target.result === 'string') {
-          this.imageSrc = new Image();
-          this.imageSrc.src = e.target.result;
-        } else {
-          this.imageSrc = e.target.result;
-        }
+    // Retorna la fecha y hora actual en formato 'YYYY-MM-DDTHH:MM'
+  }
+  isEndDateValid(): boolean {
+    const selectedDate = new Date(this.auction.endDate);
+    const currentDate = new Date();
+    return selectedDate >= currentDate;
+  }
 
-        this.renderer.setAttribute(
-          this.elementRef.nativeElement.querySelector('.file-upload-image'),
-          'src',
-          (this.imageSrc as HTMLImageElement).src ?? ''
-        );
-        this.renderer.removeClass(
-          this.elementRef.nativeElement.querySelector('.file-upload-content'),
-          'd-none'
-        );
-        this.imageName = input.files?.[0]?.name ?? ''; // Set image name
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.setDropAreaStyle(true);
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.setDropAreaStyle(false);
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.setDropAreaStyle(false);
+
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      this.loadFile(files[0]);
+    }
+  }
+
+  onClick() {
+    this.fileInput?.nativeElement.click();
+  }
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.loadFile(file);
+    }
+  }
+
+  loadFile(file: File) {
+    if (file.type.startsWith('image/')) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imageSrc = reader.result;
+        this.auction.img = file;
       };
-  
-      reader.readAsDataURL(input.files[0]);
+      reader.readAsDataURL(file);
     } else {
-      this.removeUpload();
-    }
-  }
-  
-  removeUpload() {
-    const fileUploadInput: HTMLElement | null = this.elementRef.nativeElement.querySelector('.file-upload-input');
-    if (fileUploadInput) {
-      fileUploadInput.replaceWith(fileUploadInput.cloneNode(true));
-      this.renderer.addClass(
-        this.elementRef.nativeElement.querySelector('.file-upload-content'),
-        'd-none'
-      );
-      this.renderer.removeClass(
-        this.elementRef.nativeElement.querySelector('.image-upload-wrap'),
-        'd-none'
-      );
-      this.imageSrc = null; // Clear image source
-      this.imageName = null; // Clear image name
+      alert('Por favor, carga solo imágenes');
     }
   }
 
-  triggerFileInput() {
-    this.elementRef.nativeElement.querySelector('.file-upload-input')?.click();
-  }
-
-  ngOnInit() {
-    this.elementRef.nativeElement.querySelector('.image-upload-wrap')
-      .addEventListener('dragover', () => {
-        this.renderer.addClass(
-          this.elementRef.nativeElement.querySelector('.image-upload-wrap'),
-          'image-dropping'
-        );
-      });
-
-    this.elementRef.nativeElement.querySelector('.image-upload-wrap')
-      .addEventListener('dragleave', () => {
-        this.renderer.removeClass(
-          this.elementRef.nativeElement.querySelector('.image-upload-wrap'),
-          'image-dropping'
-        );
+  onSubmit() {
+    this.auction.categories = Object.keys(this.selectedCategories).filter(
+      (category) => this.selectedCategories[category]
+    );
+    const formData = new FormData();
+    formData.append('objectName', this.auction.objectName);
+    formData.append('description', this.auction.description);
+    formData.append('categories', this.auction.categories.join(','));
+    formData.append('startDate', new Date().toISOString());
+    formData.append('endDate', this.auction.endDate);
+    formData.append('startBid', new Date(this.auction.startBid).toISOString());
+    if (this.auction.img) {
+      formData.append('img', this.auction.img);
+    }
+    const formDataObject : any = {};
+    formData.forEach((value, key) => {
+      formDataObject[key] = value;
     });
+    console.log(formDataObject);
+
+    /*  this.http.post('URL_DEL_BACKEND/guardarSubasta', formData).subscribe(response => {
+      console.log('Subasta guardada', response);
+    }); */
+  }
+
+  private setDropAreaStyle(isDragOver: boolean) {
+    const dropArea = document.querySelector('.file-drop');
+    if (dropArea) {
+      if (isDragOver) {
+        dropArea.classList.add('drag-over');
+      } else {
+        dropArea.classList.remove('drag-over');
+      }
+    }
   }
 }
